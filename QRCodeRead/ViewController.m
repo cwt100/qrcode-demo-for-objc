2119a4a0c19810e0b232239dd73a04c1b3e215c7
WanTing Cheng <cwt0000d@gmail.com>
2020-05-07 23:15:30 +0800

    initial project.

--- QRCodeRead/ViewController.m
+++ QRCodeRead/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @end
 
@@ -19,5 +19,46 @@
     // Do any additional setup after loading the view.
 }
 
+#pragma Browser
+- (IBAction)onBrowserFileClick:(id)sender {
+    UIImagePickerController *pickerController = [[UIImagePickerController alloc] init];
+    pickerController.delegate = self;
+    pickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    [self presentViewController:pickerController animated:YES completion:nil];
+}
 
+#pragma UIImagePickerController delegate
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey,id> *)info {
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    if (info == nil) {
+        NSLog(@"QRCode failed: info is nil");
+        return;
+    }
+
+    UIImage *pickedImage = [info valueForKey:UIImagePickerControllerOriginalImage];
+    if (pickedImage == nil) {
+        NSLog(@"QRCode failed: pick image failed");
+        return;
+    }
+
+    CIImage *ciImage = [pickedImage CIImage];
+    CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeQRCode
+                                              context:nil
+                                              options:@{CIDetectorAccuracy: CIDetectorAccuracyHigh}];
+    NSArray *features = [detector featuresInImage:ciImage];
+    if (features == nil) {
+        NSLog(@"QRCode failed: no qr code");
+        return;
+    }
+
+    if (features.count == 0) {
+        //TODO: QR Code decode fail.
+        NSLog(@"QRCode failed: decode fail");
+        return;
+    }
+
+    CIQRCodeFeature *qrcodeFeature = (CIQRCodeFeature *) features[0];
+    NSLog(@"QRCode feature: %@", qrcodeFeature.messageString);
+}
 @end
